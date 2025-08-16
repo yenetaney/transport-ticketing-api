498e80556984b817ba47f8a21a08bd21b9bf4ef4
Yonatan Haile <yenetaneyh@gmail.com>
2025-08-16 15:26:35 +0300

    created views for register user and add urls

--- transport_ticketing/accounts/views.py
+++ transport_ticketing/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .serializers import UserRegistrationSerializer
 
 # Create your views here.
+class RegisterUser(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
