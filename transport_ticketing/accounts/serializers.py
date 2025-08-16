0d0363ce70fdab62c737746f7bcf5432a5899da1
Yonatan Haile <yenetaneyh@gmail.com>
2025-08-16 15:38:51 +0300

    make migration

--- transport_ticketing/accounts/serializers.py
+++ transport_ticketing/accounts/serializers.py
@@ -5,6 +5,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only = True)
 
     class Meta:
+        model = User
         fields = ['username', 'email','password']
 
         def create(self, validated_data):
